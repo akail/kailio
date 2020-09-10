a3cee6ad1e4d4a10ff8ded45f6caf67e31fb03f3
Andrew Kail <andrew.a.kail@gmail.com>
2020-09-09 21:18:57 -0400

    Lock down admin interface
    Fixes #30

--- kailio/admin.py
+++ kailio/admin.py
@@ -3,17 +3,41 @@
 import os
 import uuid
 
-from flask import current_app, Markup
+from flask import abort, current_app, redirect, request, url_for, Markup
 from flask_admin.contrib.sqla import ModelView
 from flask_admin import form
 from flask_ckeditor import CKEditorField
+from flask_security import current_user
 from werkzeug.utils import secure_filename
 
-from kailio import admin, db
+from kailio import admin, db, security
 from kailio.model import User, Role, Page, Post, Tag, Image, File
 
 
-class PageAdmin(ModelView):
+class AdminModelView(ModelView):
+    """Custom Admin model view with restrictions."""
+
+    def is_accessible(self):
+        return (
+            current_user.is_active
+            and current_user.is_authenticated
+            and current_user.has_role("superuser")
+        )
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                abort(403)
+            else:
+                # login
+                return redirect(url_for("security.login", next=request.url))
+
+
+class PageAdmin(AdminModelView):
     form_overrides = dict(content=CKEditorField)
     create_template = "admin/edit.html"
     edit_template = "admin/edit.html"
@@ -21,7 +45,7 @@ class PageAdmin(ModelView):
     form_excluded_columns = ["type"]
 
 
-class RoleView(ModelView):
+class RoleView(AdminModelView):
     can_delete = False
 
 
@@ -43,7 +67,7 @@ def register_admin_pages(app):
     :param app: Flask application
     """
 
-    class ImageView(ModelView):
+    class ImageView(AdminModelView):
 
         column_list = ["image", "name", "filename", "size", "url"]
 
@@ -58,7 +82,7 @@ def register_admin_pages(app):
             )
         }
 
-    class FileView(ModelView):
+    class FileView(AdminModelView):
 
         column_list = ["file", "name", "filename", "size", "url"]
 
@@ -70,10 +94,10 @@ def register_admin_pages(app):
             )
         }
 
-    admin.add_view(ModelView(User, db.session))
+    admin.add_view(AdminModelView(User, db.session))
     admin.add_view(RoleView(Role, db.session))
     admin.add_view(PageAdmin(Page, db.session))
     admin.add_view(PageAdmin(Post, db.session))
-    admin.add_view(ModelView(Tag, db.session))
+    admin.add_view(AdminModelView(Tag, db.session))
     admin.add_view(ImageView(Image, db.session))
     admin.add_view(FileView(File, db.session))
