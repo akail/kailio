c32c71333c2083952f492a0836d1b587404fe649
Andrew Kail <andrew.a.kail@gmail.com>
2020-10-02 15:18:44 -0400

    Testing some docker stuff

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,11 @@
 #. /app/.venv/bin/activate
 flask db upgrade
 if [ "$1" == "run" ]; then
-  exec gunicorn --reload -b :5000 --access-logfile - --error-logfile - 'kailio:create_app()'
+  exec gunicorn --reload \
+    -b :5000 \
+    --access-logfile - \
+    --error-logfile - \
+    'kailio:create_app()'
 else
   flask $@
 fi
